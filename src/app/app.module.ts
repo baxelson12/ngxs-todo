79e24a57b0d295660f1821422e7189b496c372a7
baxelson12 <bradaxelson@gmail.com>
2020-02-08 17:55:19 -0500

    Add ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
-import { AppComponent } from './app.component';
+import * as fromStore from './store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot(fromStore.states),
+    NgxsReduxDevtoolsPluginModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,2 +1,18 @@
+import { State } from '@ngxs/store';
+
 export * from './actions';
 export * from './selectors';
+
+export interface TodoStateModel {
+  todos: [];
+}
+
+@State<TodoStateModel>({
+  name: 'todo-store',
+  defaults: {
+    todos: []
+  }
+})
+export class TodoState {}
+
+export const states = [TodoState];
